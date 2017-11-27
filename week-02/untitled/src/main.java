dd5bcede48fd6802ebe2b0fac843af792e5396e8
Hieu <hieuci88@gmail.com>
2017-11-27 13:04:39 +0100

    new directory structure for 2nd week and first steps with IntelliJ

---
+++ week-02/untitled/src/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+    public static void main(String[] args) {
+        int number = 5;
+        int number2 = 6;
+        float number3 = 1f;
+
+
+        System.out.println(number + number2 + number3);
+    }
+}
