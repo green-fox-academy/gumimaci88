f86c16fcbe03686078f15ab06e98fd638f48171d
Hieu <hieuci88@gmail.com>
2017-11-30 16:11:23 +0100

    Printer exercise done

--- week-02/day-2/06_Printer/src/Printer.java
+++ week-02/day-2/06_Printer/src/Printer.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class Printer {
     public static void main(String[] args) {
-        String a = "dog";
-        printer(a);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please give the number of entries: ");
+        int num = input.nextInt();
+
+        String[] result = new String[num];
+
+        for (int i = 0; i < num; i++) {
+        System.out.println("Please enter a word: ");
+        result[i] = input.next();
+    }
+    printer(result);
+}
+    public static String printer(String[] input){
+        String result = "";
+        for (int i = 0; i < input.length; i++) {
+            result = result + input[i] + " ";
+            System.out.println(result);
+        }
+        return result;
     }
 
-    public static String printer(String input) {
+    /*public static String printer(String input) {
         System.out.println(input);
         return input;
-    }
+    }*/
 }
