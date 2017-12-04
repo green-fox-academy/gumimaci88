7ac40d521b7fef6e3ba0f5a25f4bd2a0641c070a
Hieu <hieuci88@gmail.com>
2017-12-04 11:29:26 +0100

    2nd exercise done

--- week-03/day-1/02_print_each_line/src/PrintEachLine.java
+++ week-03/day-1/02_print_each_line/src/PrintEachLine.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class PrintEachLine {
     public static void main(String[] args) {
         try {
-            Path myFilePath = Paths.get(myfile.txt);
+            Path myFilePath = Paths.get("myfile.txt");
             List<String> lines = Files.readAllLines(myFilePath);
             for (String eachLineOfLines : lines) {
                 System.out.println(eachLineOfLines);
@@ -14,9 +14,5 @@ public class PrintEachLine {
         } catch (Exception e) {
             System.out.println("Unable to read file: my-file.txt");
         }
-        // Write a program that opens a file called "my-file.txt", then prints
-        // each of lines form the file.
-        // If the program is unable to read the file (for example it does not exists),
-        // then it should print an error message like: "Unable to read file: my-file.txt"
     }
 }
