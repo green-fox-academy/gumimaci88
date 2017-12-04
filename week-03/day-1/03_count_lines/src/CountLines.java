a7bae0793688c8d581b38d07cc2d6ae4b709e15a
Hieu <hieuci88@gmail.com>
2017-12-04 13:27:33 +0100

    3rd exercise done

---
+++ week-03/day-1/03_count_lines/src/CountLines.java
@@ -0,0 +1,29 @@
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Scanner;
+
+public class CountLines {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a filename: ");
+        String file = input.nextLine();
+        numberoflines(file);
+
+    }
+
+    private static Integer numberoflines(String input) {
+        int numberLines = 0;
+        try {
+            Path filePath = Paths.get(input);
+            List<String> lines = Files.readAllLines(filePath);
+            numberLines = lines.size();
+            System.out.println(numberLines);
+        } catch (Exception e) {
+            System.out.println("File not found.");
+        }
+        return numberLines;
+    }
+}
