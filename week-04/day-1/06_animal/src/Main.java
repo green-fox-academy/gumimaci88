810673d9a07f612f26bba2641836b9f0c2a5223d
Hieu <hieuci88@gmail.com>
2017-12-11 17:25:55 +0100

    6th exercise done

---
+++ week-04/day-1/06_animal/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+  public static void main(String[] args) {
+    Animal cheetah = new Animal();
+
+    cheetah.whatdoesthefoxsay();
+
+    cheetah.play();
+    cheetah.play();
+    cheetah.eat();
+    cheetah.drink();
+    cheetah.play();
+    cheetah.play();
+    cheetah.eat();
+    cheetah.eat();
+    cheetah.drink();
+    cheetah.play();
+    cheetah.play();
+
+    cheetah.whatdoesthefoxsay();
+  }
+}
