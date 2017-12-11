65f5aea899e2e322b2109814a86f6c2c05326597
Hieu <hieuci88@gmail.com>
2017-12-11 15:50:53 +0100

    3rd exercise refactor

--- week-04/day-1/03_domino/src/sortDomino.java
+++ week-04/day-1/03_domino/src/sortDomino.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class sortDomino {
   public static void main(String[] args) {
     List<Domino> dominoes = initializeDominoes();
 
