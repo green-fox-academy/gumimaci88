c8e1d040948c412a442f007d6957900a16337c7e
Hieu <hieuci88@gmail.com>
2018-01-04 16:39:22 +0100

    7th exercise: CreditCard done

---
+++ week-06/day-1/07_credit_card/src/main/java/creditcards/CreditCardApp.java
@@ -0,0 +1,11 @@
+package main.java.creditcards;
+
+public class CreditCardApp {
+  public static void main(String[] args) {
+    ListCreator listOfCards = new ListCreator(10);
+
+    for (CreditCard card : listOfCards.list) {
+      System.out.println(card);
+    }
+  }
+}
---
+++ week-06/day-1/07_credit_card/src/main/java/creditcards/CreditCardy.java
@@ -0,0 +1,14 @@
+package main.java.creditcards;
+
+public interface CreditCardy {
+
+  int getSumCVV();
+
+  String getNameCardholder();
+
+  String getCodeAccount();
+
+  int computeSumCVV(String codeAccount);
+
+  String toString ();
+}
