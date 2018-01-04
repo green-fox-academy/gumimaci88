ca56e006951e9bc77d3a2c29cdee60eae0652214
Hieu <hieuci88@gmail.com>
2018-01-04 15:20:58 +0100

    6th Exercise: Reservations done

---
+++ week-06/day-1/06_reservations/src/Main.java
@@ -0,0 +1,12 @@
+import main.java.Reservations.Reservation;
+
+public class Main {
+  public static void main(String[] args) {
+    int reservationAmount = 10;
+
+    for (int i = 0; i < reservationAmount; i++) {
+      Reservation reservation = new Reservation();
+      System.out.println(String.format("Booking# %s for %s", reservation.getCodeBooking(), reservation.getDowBooking()));
+    }
+  }
+}
---
+++ week-06/day-1/06_reservations/src/main/java/Reservations/Reservations.java
@@ -0,0 +1,7 @@
+package main.java.Reservations;
+
+public interface Reservations {
+  String getDowBooking();
+
+  String getCodeBooking();
+}
