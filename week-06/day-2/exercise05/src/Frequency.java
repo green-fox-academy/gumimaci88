d476236651c92358529bfb8ef2aa91f036fa1d5b
Hieu <hieuci88@gmail.com>
2018-01-05 11:00:27 +0100

    5th exercise refactor, new method

--- week-06/day-2/exercise05/src/Frequency.java
+++ week-06/day-2/exercise05/src/Frequency.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Frequency {
@@ -7,5 +9,29 @@ public class Frequency {
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2));
 
     System.out.println(numbers.stream().collect(Collectors.groupingBy(s -> s, Collectors.counting())));
+
+    Map<Integer, Long> frequencyList = numbers
+            .stream()
+            .collect((Collectors
+            .groupingBy(n -> n, Collectors.counting())));
+
+    System.out.println(frequencyList);
+    System.out.println(frequencyOfNumbers(numbers));
+  }
+
+  public static Map<Integer, Integer> frequencyOfNumbers(ArrayList<Integer> inputList) {
+    Map<Integer, Integer> outputMap = new HashMap<>();
+
+    for (int i = 0; i < inputList.size(); i++) {
+      int count = 0;
+      for (int j = 0; j < inputList.size(); j++) {
+        if (inputList.get(i) == inputList.get(j)) {
+          count++;
+        }
+      }
+      outputMap.put(inputList.get(i), count);
+    }
+
+    return outputMap;
   }
 }
