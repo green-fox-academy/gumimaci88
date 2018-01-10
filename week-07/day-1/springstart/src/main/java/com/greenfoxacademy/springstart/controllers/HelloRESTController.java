b11ebc15b1b3a5de2c86cccbf9f77999db2f76f1
Hieu <hieuci88@gmail.com>
2018-01-10 16:12:24 +0100

    HelloREST completed

--- week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,14 +1,22 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class HelloRESTController {
 
+  AtomicLong counter = new AtomicLong(1);
+
   @RequestMapping(value = "/greeting")
-  public Greeting greeting() {
-    Greeting hello = new Greeting(1, "Hello World!");
+  public Greeting greeting(@RequestParam(value = "name", required = false) String name) {
+    if (name == null) {
+      name = "World";
+    }
+    Greeting hello = new Greeting(counter.incrementAndGet(), "Hello, " + name);
     return hello;
   }
 }
