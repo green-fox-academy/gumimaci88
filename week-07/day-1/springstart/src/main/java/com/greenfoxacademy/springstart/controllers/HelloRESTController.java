07e1d0853b859652482dc7a3bc955c5d78d60267
Hieu <hieuci88@gmail.com>
2018-01-11 15:35:08 +0100

    Hello Web App finished

--- week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class HelloRESTController {
 
-  AtomicLong counter = new AtomicLong(1);
+  AtomicLong counter = new AtomicLong(0);
 
   @RequestMapping(value = "/greeting")
   public Greeting greeting(@RequestParam(value = "name", required = false) String name) {
