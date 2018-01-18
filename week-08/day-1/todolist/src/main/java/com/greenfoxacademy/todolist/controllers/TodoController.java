6b9005f6365e181662f728b7ad74af6a0a42a863
Hieu <hieuci88@gmail.com>
2018-01-18 11:46:39 +0100

    Refactor controller

--- week-08/day-1/todolist/src/main/java/com/greenfoxacademy/todolist/controllers/TodoController.java
+++ week-08/day-1/todolist/src/main/java/com/greenfoxacademy/todolist/controllers/TodoController.java
@@ -1,20 +1,28 @@
 package com.greenfoxacademy.todolist.controllers;
 
+import com.greenfoxacademy.todolist.models.ToDo;
 import com.greenfoxacademy.todolist.repositories.ToDoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
+@RequestMapping("/todo")
 public class TodoController {
 
   @Autowired
   ToDoRepository toDoRepository;
 
-  @RequestMapping(value = {"/", "/list"})
+  @GetMapping(value = {"/", "/list"})
   public String list(Model model) {
-    model.addAttribute("todos", toDoRepository.findAll());
+    List<ToDo> todosList = new ArrayList<>();
+    toDoRepository.findAll().forEach(todosList::add);
+    model.addAttribute("todos", todosList);
     return "todoslist";
   }
 }
