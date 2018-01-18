0a7d4344bf6cc670a1fbfff5caeb46fd88f34b3c
Hieu <hieuci88@gmail.com>
2018-01-18 10:40:12 +0100

    ToDoList first exercise refactor

--- week-08/day-1/todolist/src/main/java/com/greenfoxacademy/todolist/ToDoListApplication.java
+++ week-08/day-1/todolist/src/main/java/com/greenfoxacademy/todolist/ToDoListApplication.java
@@ -2,26 +2,27 @@ package com.greenfoxacademy.todolist;
 
 import com.greenfoxacademy.todolist.models.ToDo;
 import com.greenfoxacademy.todolist.repositories.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ToDoListApplication {
+public class ToDoListApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner listToDos(ToDoRepository toDoRepository) {
-	  return (String... args) -> {
+	@Autowired
+	ToDoRepository toDoRepository;
+
+	@Override
+	public void run(String... strings) throws Exception{
 	    toDoRepository.save(new ToDo("I have to learn Object Relational Mapping"));
       toDoRepository.save(new ToDo("Get a good sleep."));
       toDoRepository.save(new ToDo("Finish exercises."));
       toDoRepository.save(new ToDo("Understand the material."));
-    };
-  }
+    }
 
 }
